cd2d98d25b445e1c38fd74e70d3320503381b4cb
Stefan <jovanovics399@gmail.com>
2023-07-17 20:15:25 +0100

    Starting Spotify clone

---
+++ src/pages/Discover.jsx
@@ -0,0 +1,14 @@
+import { Error, Loader, songCard} from '../components'
+import {genres} from '../assets/constants'
+
+const Discover = () => {
+    return (
+        <div className='flex felx-col'>
+            <div className='flex'>
+
+            </div>
+        </div>
+    )
+};
+
+export default Discover;
