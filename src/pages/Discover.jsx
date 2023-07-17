3144cb4c7d95ced8150986d728bbc9bcca558987
Stefan <jovanovics399@gmail.com>
2023-07-17 22:13:56 +0100

    installed tailwind

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -1,11 +1,16 @@
-import { Error, Loader, songCard} from '../components'
+import { Error, Loader, SongCard} from '../components'
 import {genres} from '../assets/constants'
 
 const Discover = () => {
+    console.log(genres)
     return (
-        <div className='flex felx-col'>
-            <div className='flex'>
-
+        <div className='flex flex-col'>
+            <div className='w-full flex justify-between items-center
+            sm:flex-row flex-col mt-4 mb-10'>
+                <h2 className='font-bold text-3x1 text-white text-left'>
+                Discover{genreTitle}
+                </h2>
+                <select name="" id=""></select>
             </div>
         </div>
     )
